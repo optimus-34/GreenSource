a8e2e1b2a06412e5f5a2b0ba8d94eecb102e0252
optimus-34 <paxorion-34@gmail.com>
2024-10-29 18:03:12 +0530

    added auth-service

---
+++ auth-service/src/app.ts
@@ -0,0 +1,27 @@
+// src/app.ts
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/userRoutes';
+import connectDB from './config/database';
+
+
+const app = express();
+
+// Connect to the database
+connectDB()
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Define routes
+app.use('/auth', authRoutes);
+
+// Health check route
+app.get('/', (req, res) => {
+    res.send('Auth service is running');
+});
+
+export default app;
